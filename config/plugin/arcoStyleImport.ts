eaf771e390d958e7512d8b8d9572c0f75019ed44
Zian Wang <rblueskyt@gmail.com>
2023-06-13 00:26:51 +0100

    feat: project Magpie was initialised

--- config/plugin/arcoStyleImport.ts
+++ config/plugin/arcoStyleImport.ts
@@ -1,6 +1,5 @@
 /**
  * Theme import
- * 样式按需引入
  * https://github.com/arco-design/arco-plugins/blob/main/packages/plugin-vite-vue/README.md
  * https://arco.design/vue/docs/start
  */
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Magpie Front End
+
+This platform is developed for researching the effectiveness of the existing APP fraud warnings.
+The front end is powered by ArcoDesign.
