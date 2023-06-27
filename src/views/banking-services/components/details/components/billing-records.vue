0ec84f8c4f400c52ddb09160d93e3b3f0c6c2a98
Zian Wang <rblueskyt@gmail.com>
2023-06-27 06:09:36 +0100

    feat: billing records implementation part 2

--- src/views/banking-services/components/details/components/billing-records.vue
+++ src/views/banking-services/components/details/components/billing-records.vue
@@ -1,5 +1,99 @@
 <template>
-  <div class="billing-records">test</div>
+  <div class="billing-records">
+    <a-tabs size="large" type="rounded" :justify="true" :animation="true">
+      <a-tab-pane key="ALL" title="ALL" :disabled="billingRecordsLoading">
+        <a-list size="small" :bordered="false" :max-height="681" hoverable>
+          <a-list-item
+            v-for="(itemRecord, indexRecord) in billingRecords[
+              Number(focusedAccountIdx)
+            ].all"
+            :key="indexRecord"
+            :class="itemRecord.title ? 'date-of-records' : 'record-content'"
+            @click="test()"
+          >
+            <div v-if="itemRecord.title">
+              {{ itemRecord.title }}
+            </div>
+            <a-list-item-meta
+              v-if="!itemRecord.title"
+              :title="itemRecord.payee"
+            >
+              <template #avatar>
+                <a-avatar
+                  v-if="
+                    itemRecord.type === 'Out' && itemRecord.pending === false
+                  "
+                  shape="circle"
+                  style="
+                    border: 1px solid #f73131;
+                    color: #f73131;
+                    background-color: white;
+                  "
+                >
+                  {{ itemRecord.type }}
+                </a-avatar>
+                <a-avatar
+                  v-else-if="
+                    itemRecord.type === 'In' && itemRecord.pending === false
+                  "
+                  shape="circle"
+                  style="
+                    border: 1px solid #00b42a;
+                    color: #00b42a;
+                    background-color: white;
+                  "
+                >
+                  {{ itemRecord.type }}
+                </a-avatar>
+                <a-avatar
+                  v-else
+                  shape="circle"
+                  style="
+                    border: 1px dashed #86909c;
+                    color: #86909c;
+                    background-color: white;
+                  "
+                >
+                  {{ itemRecord.type }}
+                </a-avatar>
+              </template>
+            </a-list-item-meta>
+            <template v-if="!itemRecord.title" #extra>
+              <div
+                class="amount-and-balance"
+                style="height: 100%; display: flex"
+              >
+                <a-space direction="vertical" :size="0" fill>
+                  <a-typography-text
+                    v-if="itemRecord.type === 'Out'"
+                    style="font-size: 16px; line-height: 1"
+                  >
+                    -£{{ numberFormatter(itemRecord.amount.toString()) }}
+                  </a-typography-text>
+                  <a-typography-text
+                    v-else
+                    type="success"
+                    style="font-size: 16px; line-height: 1"
+                  >
+                    £{{ numberFormatter(itemRecord.amount.toString()) }}
+                  </a-typography-text>
+                  <a-typography-text type="secondary" style="line-height: 1">
+                    £{{ numberFormatter(itemRecord.balance.toString()) }}
+                  </a-typography-text>
+                </a-space>
+              </div>
+            </template>
+          </a-list-item>
+        </a-list>
+      </a-tab-pane>
+      <a-tab-pane key="IN" title="IN" :disabled="billingRecordsLoading">
+        Content of Tab Panel 2
+      </a-tab-pane>
+      <a-tab-pane key="OUT" title="OUT" :disabled="billingRecordsLoading">
+        Content of Tab Panel 3
+      </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -10,6 +104,7 @@
     queryBillingRecordListIn,
     queryBillingRecordListOut,
   } from '@/api/banking-services';
+  import numberFormatter from '@/utils/number-formatter';
 
   const focusedAccountIdx: any = inject('focusedAccountIdx');
   const focusedAccount: any = inject('focusedAccount');
@@ -30,9 +125,16 @@
       };
       const resData = await queryBillingRecordListAll(reqData);
       if ((resData as any).code === 20000) {
-        billingRecords.value[accountIdx].all = billingRecords.value[
-          accountIdx
-        ].all.concat(resData.data.dailyRecords);
+        let convertedResData: any = [];
+        for (let i = 0; i < resData.data.dailyRecords.length; i += 1) {
+          const collectionTitle = { title: resData.data.dailyRecords[i].date };
+          convertedResData.push(collectionTitle);
+          convertedResData = convertedResData.concat(
+            resData.data.dailyRecords[i].records
+          );
+        }
+        billingRecords.value[accountIdx].all =
+          billingRecords.value[accountIdx].all.concat(convertedResData);
       } else {
         Message.error(
           'Sorry, the billing records cannot be retrieved at this time'
@@ -51,9 +153,16 @@
       };
       const resData = await queryBillingRecordListIn(reqData);
       if ((resData as any).code === 20000) {
-        billingRecords.value[accountIdx].in = billingRecords.value[
-          accountIdx
-        ].in.concat(resData.data.dailyRecords);
+        let convertedResData: any = [];
+        for (let i = 0; i < resData.data.dailyRecords.length; i += 1) {
+          const collectionTitle = { title: resData.data.dailyRecords[i].date };
+          convertedResData.push(collectionTitle);
+          convertedResData = convertedResData.concat(
+            resData.data.dailyRecords[i].records
+          );
+        }
+        billingRecords.value[accountIdx].in =
+          billingRecords.value[accountIdx].in.concat(convertedResData);
       } else {
         Message.error(
           'Sorry, the billing records cannot be retrieved at this time'
@@ -72,9 +181,16 @@
       };
       const resData = await queryBillingRecordListOut(reqData);
       if ((resData as any).code === 20000) {
-        billingRecords.value[accountIdx].out = billingRecords.value[
-          accountIdx
-        ].out.concat(resData.data.dailyRecords);
+        let convertedResData: any = [];
+        for (let i = 0; i < resData.data.dailyRecords.length; i += 1) {
+          const collectionTitle = { title: resData.data.dailyRecords[i].date };
+          convertedResData.push(collectionTitle);
+          convertedResData = convertedResData.concat(
+            resData.data.dailyRecords[i].records
+          );
+        }
+        billingRecords.value[accountIdx].out =
+          billingRecords.value[accountIdx].out.concat(convertedResData);
       } else {
         Message.error(
           'Sorry, the billing records cannot be retrieved at this time'
@@ -97,8 +213,44 @@
       currentPage.value[accountIdx].out = 1;
     }
   };
+  // TODO: REMOVE
+  const test = () => {
+    console.log('yes');
+  }
 
   getBillingRecords();
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .billing-records {
+    height: 100%;
+    padding-top: 16px;
+  }
+
+  .billing-records :deep(.arco-tabs-nav-tab-list) {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding: 0px 6px;
+  }
+
+  .billing-records :deep(.arco-tabs-tab) {
+    width: 140px;
+    justify-content: center;
+  }
+
+  .billing-records :deep(.arco-list) {
+    border-radius: 0px;
+    border-top: 1px solid var(--color-neutral-3);
+    border-bottom: 1px solid var(--color-neutral-3);
+  }
+
+  .date-of-records {
+    background-color: #f2f3f5;
+  }
+
+  .amount-and-balance :deep(.arco-space-fill) {
+    justify-content: space-between;
+    align-items: end;
+  }
+</style>
