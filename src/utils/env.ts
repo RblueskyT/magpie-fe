21cba4c2cc90aa89cf989ce98d1c56f8b83c0540
Zian Wang <rblueskyt@gmail.com>
2023-07-21 00:38:48 +0100

    fix: mock.js is not enabled for the production environment

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,4 @@
-const debug = import.meta.env.MODE !== 'production';
+// const debug = import.meta.env.MODE !== 'production';
+const debug = true; // enable mock.js for the production environment
 
 export default debug;
